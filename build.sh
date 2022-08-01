c58409ebc97642218516dfd058d53349a5ae824a
Matheus Benachio <matheuslopesbenachio@gmail.com>
2022-08-01 13:24:42 -0300

    fix: general fixes for build and dockerfiles

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-TAGOCOREVERSION=$arg
+FULL_VERSION=$1
 
-# docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag your-username/multiarch-example:buildx-latest .
-# docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag your-username/multiarch-example:buildx-latest .
+SPLIT=(${FULL_VERSION//./ })
+MAJOR=${SPLIT[0]}
+MINOR=${SPLIT[1]}
+PATCH=${SPLIT[2]}
 
-
-# # Alpine
-# cd Alpine; docker buildx build --platform linux/arm64/v8,linux/amd64 --tag your-username/multiarch-example:buildx-latest .
+# Alpine
+cd Alpine;
+docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
+    --platform linux/arm64/v8,linux/amd64 \
+    --tag tago-io/tagocore:alpine \
+    --tag tago-io/tagocore:${MAJOR}.${MINOR}-alpine  \
+    --tag tago-io/tagocore:${MAJOR}.${MINOR}.${PATCH}-alpine .;
+cd ..;
 
 # Linux
-cd Linux; docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag tagoio/tagocore:buildx-latest --tag tagoio/tagocore:0.6.0-buster --tag tagoio/tagocore:0-buster .
+cd Linux;
+docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
+    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+    --tag tago-io/tagocore \
+    --tag tago-io/tagocore:jammy \
+    --tag tago-io/tagocore:${MAJOR}.${MINOR}-jammy  \
+    --tag tago-io/tagocore:${MAJOR}.${MINOR}.${PATCH}-jammy .
