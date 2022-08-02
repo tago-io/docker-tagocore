1b2e40da0f6870adb7a4535212ded996c615f020
Matheus Benachio <matheuslopesbenachio@gmail.com>
2022-08-02 11:01:26 -0300

    feat: fixed build.sh in workflow and in-file

--- build.sh
+++ build.sh
@@ -7,25 +7,23 @@ MAJOR=${SPLIT[0]}
 MINOR=${SPLIT[1]}
 PATCH=${SPLIT[2]}
 
-ECHO $FULL_VERSION
-
 # Alpine
-# cd alpine;
-# docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
-#     --platform linux/arm64/v8,linux/amd64 \
-#     --tag tagoio/tagocore:alpine \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}-alpine  \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH}-alpine .;
-# cd ..;
+cd alpine;
+docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
+    --platform linux/arm64/v8,linux/amd64 \
+    --tag tagoio/tagocore:alpine \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}-alpine  \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH}-alpine .;
+cd ..;
 
-# # Linux
-# cd debian;
-# docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
-#     --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-#     --tag tagoio/tagocore \
-#     --tag tagoio/tagocore:debian \
-#     --tag tagoio/tagocore:bullseye \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}-bullseye  \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH}-bullseye \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}  \
-#     --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH} .
+# Debian
+cd debian;
+docker buildx build --push --build-arg TAGOCORE_VERSION=${FULL_VERSION} \
+    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+    --tag tagoio/tagocore \
+    --tag tagoio/tagocore:debian \
+    --tag tagoio/tagocore:bullseye \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}-bullseye  \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH}-bullseye \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}  \
+    --tag tagoio/tagocore:${MAJOR}.${MINOR}.${PATCH} .
